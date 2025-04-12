cf44519571639ef9d325e30cd6af99abb8e36700
Miguel Angel <miguel43155957@gmail.com>
2025-04-11 20:58:14 -0500

    issues - correctionsv4

--- app/app.py
+++ app/app.py
@@ -42,8 +42,4 @@ def index():
 
 
 if __name__ == "__main__":  # pragma: no cover
-    app.run(
-        debug=True,
-        port=5000,
-        host="0.0.0.0"
-    )  # Quita debug=True para producción
+    app.run(debug=True, port=5000, host="0.0.0.0")
