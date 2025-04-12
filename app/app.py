7d7f54aa09bf17e9ed44deed0d69165163e17b88
Miguel Angel <miguel43155957@gmail.com>
2025-04-11 20:30:03 -0500

    issues - corrections

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 # app/app.py
+""" 
+Importa los módulos de Flask y las funciones de operaciones básicas definidas en 'calculadora'.
+"""
 from flask import Flask, render_template, request
 from .calculadora import sumar, restar, multiplicar, dividir
 
@@ -7,6 +10,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """ 
+    Este método gestiona la página principal: procesa formularios
+    POST para realizar operaciones aritméticas, maneja errores y muestra el resultado.
+    """
     resultado = None
     if request.method == "POST":
         try:
@@ -33,4 +40,8 @@ def index():
 
 
 if __name__ == "__main__":  # pragma: no cover
-    app.run(debug=True, port=5000, host="0.0.0.0")  # Quita debug=True para producción
+    app.run(
+        debug=True,
+        port=5000,
+        host="0.0.0.0"
+    )  # Quita debug=True para producción
--- app/calculadora.py
+++ app/calculadora.py
@@ -1,17 +1,24 @@
 # app/calculadora.py
+"""Módulo que proporciona funciones básicas de cálculo: suma, resta, multiplicación y división."""
 def sumar(a, b):
+    """Devuelve la suma de dos números."""
     return a + b
 
-
+"""Módulo que proporciona funciones básicas de cálculo: suma, resta, multiplicación y división."""
 def restar(a, b):
+    """Devuelve la resta de dos números."""
     return a - b
 
-
+"""Módulo que proporciona funciones básicas de cálculo: suma, resta, multiplicación y división."""
 def multiplicar(a, b):
+    """Devuelve la multiplicacion de dos números."""
     return a * b
 
-
+"""Módulo que proporciona funciones básicas de cálculo: suma, resta, multiplicación y división."""
 def dividir(a, b):
+    """Devuelve el resultado de dividir 'a' entre 'b'.
+    Lanza una excepción ZeroDivisionError si 'b' es cero.
+    """
     if b == 0:
         raise ZeroDivisionError("No se puede dividir por cero")
     return a / b
